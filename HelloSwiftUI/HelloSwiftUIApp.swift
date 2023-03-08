2ddc76a50b57424fe7219c5be6968a7150fff950
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-08 09:34:54 +0900

    add files

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 岡本 翔真 on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
